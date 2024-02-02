7f5b831111575f17606611ef6264c0d3f695c8a6
aslanchan7 <124756060+aslanchan7@users.noreply.github.com>
2024-02-01 16:48:39 -0800

    Fixed ArrayTests.java

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -1,11 +1,11 @@
 
-
 public class ArrayExamples {
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+    int[] newArr = arr.clone();
+    for (int i = 0; i < arr.length; i += 1) {
+      arr[i] = newArr[arr.length - 1 - i];
     }
   }
 
@@ -13,28 +13,32 @@ public class ArrayExamples {
   // order
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
-    for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+    for (int i = 0; i < arr.length; i += 1) {
+      newArray[i] = arr[arr.length - 1 - i];
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
   // lowest number when calculating. Returns 0 if there are no elements or just
   // 1 element in the array
   static double averageWithoutLowest(double[] arr) {
-    if(arr.length < 2) { return 0.0; }
+    if (arr.length < 2) {
+      return 0.0;
+    }
     double lowest = arr[0];
-    for(double num: arr) {
-      if(num < lowest) { lowest = num; }
+    for (double num : arr) {
+      if (num < lowest) {
+        lowest = num;
+      }
     }
     double sum = 0;
-    for(double num: arr) {
-      if(num != lowest) { sum += num; }
+    for (double num : arr) {
+      if (num != lowest) {
+        sum += num;
+      }
     }
     return sum / (arr.length - 1);
   }
 
-
 }
-
